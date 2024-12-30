db7d4796228dc16f00a7269516857984304a519a
Test5 <test5@medis.local>
2024-12-30 12:22:59 +0330

    create sample infrastructure

---
+++ src/Application/Sample.Application/Users/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using Sample.Application.Users.Dtos;
+using Sample.Commons.Interfaces;
+
+namespace Sample.Application.Users.Services;
+
+public interface IUserService : IService
+{
+    Task<long> Add(AddUserDto dto);
+    Task<IPageResult<GetAllUsersDto>> GetAllUsers(IPagination? pagination=null);
+}
